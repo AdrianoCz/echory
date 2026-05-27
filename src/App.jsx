b13a16785d647ae8420602ecffedcc07cd4f9c60
Pilar-cmd <adrianozx2007@gmail.com>
2026-05-27 10:48:16 -0300

    add initial nav design

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import { useState, useRef } from 'react'
+import { BrowserRouter, Routes, Route, Link, NavLink, Router} from 'react-router-dom'
 import './App.css'
+import Logo from "./logo.jsx"
+import userIcon from "/user.png"
 
 function App() {
+    
+    return(<BrowserRouter>
+        <nav>
+            <Link to="/">
+            <Logo />
+            </Link>
+            <div>
+                <div className='searchBar'></div>
+                <img className='userIcon' src={userIcon} alt="" />
+            </div>
+        </nav>
+
+        
+        </BrowserRouter>
+    )
+    
 }
 
 export default App
